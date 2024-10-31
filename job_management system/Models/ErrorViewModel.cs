373e04f5e4ed12dec379de5165fdd7dead2eb8d2
Your Moha_Hareri <mcabdi578@gmail.com>
2024-10-31 11:05:19 +0300

    first commit

---
+++ job_management system/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace job_management_system.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
